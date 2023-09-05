52a3fce0f25fcf3b878d81d34bafabf00ec98714
Maria Noor <maria@roboamp.com>
2023-09-05 17:43:09 +0500

    added authentication component

---
+++ src/components/Authentication/Layout.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from '../../../public/images/logo.png';
+
+export default function Layout({children}) {
+    return (
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <Link
+                    href="#"
+                    className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+                >
+                    <Image
+                        src={logo}
+                        alt="Vercel Logo"
+                        className="w-12 h-12 mr-4"
+                        width={100}
+                        height={24}
+                    />
+                    Chit Chat Cirus
+                </Link>
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    {children}
+                </div>
+            </div>
+        </section>
+    );
+}
